0afd38d47693079213afc5aeb7da5b3f544c7e04
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-03-18 17:08:32 +0900

    refactor: and -> && for MSVC

--- sources/grcc.cc
+++ sources/grcc.cc
@@ -3967,7 +3967,7 @@ Bool MGraph::visit(int nd)
     }
     nodes[nd]->visited = 0;
     for (td = 0; td < nNodes; td++) {
-        if ((adjMat[nd][td] > 0) and (nodes[td]->visited < 0)) {
+        if ((adjMat[nd][td] > 0) && (nodes[td]->visited < 0)) {
             if (visit(td)) {
                 return True;
             }
