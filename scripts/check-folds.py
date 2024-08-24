ae6025d17c30af2388a230fe7540e16b8e416b85
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-08-24 20:52:36 +0900

    tmp

--- scripts/check-folds.py
+++ scripts/check-folds.py
@@ -1,32 +1,37 @@
 import argparse
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class Fold:
     lineno: int
     label: str
 
+
 def check_file(filename: str, fix: bool) -> None:
     fixed = False
     lineno = 0
     folds = []  # as a stack
+    print(filename)
     pass
+
 
 def main() -> None:
     """Main entry point."""
-    parser = argparse.ArgumentParser(usage='%(prog)s [options] files..')
+    parser = argparse.ArgumentParser(usage="%(prog)s [options] files..")
     parser.add_argument(
-        '-f', '--fix', action='store_true', default=False,
-        help='close all folds'
+        "-f", "--fix", action="store_true", default=False, help="close all folds"
     )
-    parser.add_argument('filenames', nargs='*', help=argparse.SUPPRESS)
+    parser.add_argument("filenames", nargs="*", help=argparse.SUPPRESS)
     args = parser.parse_args()
     files = args.filenames
     if not files:
         parser.print_help()
     else:
+        print('start:')
         for f in files:
             check_file(f, args.fix)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
