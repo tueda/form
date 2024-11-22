33a76e158db71c1101b668b318c7869af7f9af01
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-11-22 22:32:20 +0900

    test(check.rb): relax warning detection pattern
    The new pattern detects warnings printed in a non-canonical form.
    
    WIP: Close 581.

--- check/check.rb
+++ check/check.rb
@@ -689,9 +689,9 @@ module FormTest
   # true if the FORM job put warning messages.
   def warning?(expected_message = nil)
     if expected_message.nil?
-      @stdout =~ /(^|\R)\S+ Line \d+ --> Warning/
+      @stdout =~ /Warning:/
     else
-      @cleaned_stdout =~ Regexp.new("(^|\\R)\\S+ Line \\d+ --> Warning: .*#{Regexp.escape(expected_message)}")
+      @cleaned_stdout =~ Regexp.new("Warning: .*#{Regexp.escape(expected_message)}")
     end
   end
 
