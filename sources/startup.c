9c6298561d7ac538565f851ea1e67bfaf52cc531
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-12-28 10:56:55 +0900

    debug

--- sources/startup.c
+++ sources/startup.c
@@ -1728,7 +1728,6 @@ VOID CleanUp(WORD par)
 
 	if ( FG.fname ) {
 	CleanUpSort(0);
-	return;
 	for ( i = 0; i < 3; i++ ) {
 		if ( AR.Fscr[i].handle >= 0 ) {
 			if ( AR.Fscr[i].name ) {
@@ -1743,7 +1742,6 @@ VOID CleanUp(WORD par)
 			AR.Fscr[i].POfill = 0;
 		}
 	}
-	return;
 	if ( par > 0 ) {
 /*
 	Close all input levels above the lowest?
