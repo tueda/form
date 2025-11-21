c3c4e62b2449e75b2ee9a2b61b5669c9ab4fbce4
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2025-11-21 17:09:37 +0900

    test(check.rb): define TEST* environment variables
    The environment variables TESTFILE, TESTFILEDIR, TESTCASE and TESTTMPDIR
    are now available for use in FORM test cases.

--- check/check.rb
+++ check/check.rb
@@ -438,6 +438,10 @@ module FormTest
       begin
         nfiles.times do |i|
           ENV["FORM"] = FormTest.cfg.form_cmd
+          ENV["TESTFILE"] = info.full_filename
+          ENV["TESTFILEDIR"] = File.dirname(info.full_filename)
+          ENV["TESTCASE"] = info.classname
+          ENV["TESTTMPDIR"] = @tmpdir
           @filename = "#{i + 1}.frm"
           execute("#{ulimits}#{FormTest.cfg.form_cmd} #{@filename}")
           if !finished?
@@ -841,6 +845,7 @@ end
 class TestInfo
   def initialize
     @classname = nil
+    @full_filename = nil
     @where = nil    # where the test is defined
     @foldname = nil # fold name of the test
     @enabled = nil  # enabled or not
@@ -851,7 +856,7 @@ class TestInfo
     @times = nil    # elapsed time (array)
   end
 
-  attr_accessor :classname, :where, :foldname, :enabled, :sources, :time_dilation,
+  attr_accessor :classname, :full_filename, :where, :foldname, :enabled, :sources, :time_dilation,
                 :status, :times
 
   # Return the description of the test.
@@ -890,6 +895,7 @@ class TestCases
   # Convert a .frm file to a .rb file and load it.
   def make_ruby_file(filename)
     # Check existing files.
+    full_filename = File.expand_path(filename)
     inname = File.basename(filename)
     outname = "#{File.basename(filename, '.frm')}.rb"
     if @files.include?(outname)
@@ -932,6 +938,7 @@ class TestCases
               @classes.push(classname)
               @classes_info["Test_#{classname}"] = info
               info.classname = classname
+              info.full_filename = full_filename
               info.where = "#{inname}:#{lineno}"
               info.foldname = fold
               info.enabled = test_enabled?(classname)
